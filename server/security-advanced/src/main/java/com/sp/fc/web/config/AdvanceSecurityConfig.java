29ece45ab558372dad1f8e66f84a15504ce0391d
seheon <quwe153@naver.com>
2022-08-20 13:13:26 +0900

    [STUDY] JWT 토큰

---
+++ server/security-advanced/src/main/java/com/sp/fc/web/config/AdvanceSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.sp.fc.web.config;
+
+import com.sp.fc.user.service.SpUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AdvanceSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private SpUserService userService;
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // 로그인 필터는 authenticationManager에게 인증 위임
+        JWTLoginFilter loginFilter = new JWTLoginFilter(authenticationManager());  // 로그인 필터
+
+        // 체크 필터는 사용자를 직접 가져와야 될 수도 있음
+        JWTCheckFilter checkFilter = new JWTCheckFilter(authenticationManager(), userService);  // request 마다 검증 필터
+
+        http
+                .csrf().disable()  // 비효율
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 안씀
+                )
+                .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAt(checkFilter, BasicAuthenticationFilter.class);
+    }
+}
